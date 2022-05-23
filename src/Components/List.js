237c923dd8c954584b03434acbb50d01fedc2c4d
Osama <012oO34567892015@gmail.com>
2022-05-23 15:44:36 +0200

    App-Quiz

---
+++ src/Components/List.js
@@ -0,0 +1,25 @@
+const List = [
+    {
+      id: 1,
+      questions: 'كم عمري',
+      answer1:"20",
+      answer2:"19",
+      answer3:"22"
+    },
+    {
+        id: 2,
+        questions: 'اسمك؟',
+        answer1:"اسامة",
+        answer2:"عمر",
+        answer3:"فرح"
+      },
+      {
+        id: 3,
+        questions: 'دولة؟',
+        answer1:"الجزائر",
+        answer2:"مصر",
+        answer3:"عراق"
+      },
+  ]
+  export default List
+  
